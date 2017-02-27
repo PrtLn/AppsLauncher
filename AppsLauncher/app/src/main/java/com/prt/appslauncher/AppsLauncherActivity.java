7936732e435c8c7d52e0c7ac562c184d818eb451
oleno <pirit.lena@gmail.com>
2017-02-27 15:14:39 +0200

    initial

---
+++ AppsLauncher/app/src/main/java/com/prt/appslauncher/AppsLauncherActivity.java
@@ -0,0 +1,13 @@
+package com.prt.appslauncher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppsLauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apps_launcher);
+    }
+}
