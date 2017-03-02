90625fb5b2458dd751bb0f4d4117b7d10772a4a8
oleno <pirit.lena@gmail.com>
2017-03-02 13:58:48 +0200

    creating explicit intents at runtime

--- AppsLauncher/app/src/main/java/com/prt/appslauncher/AppsLauncherFragment.java
+++ AppsLauncher/app/src/main/java/com/prt/appslauncher/AppsLauncherFragment.java
@@ -1,6 +1,7 @@
 package com.prt.appslauncher;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
@@ -48,13 +49,15 @@ public class AppsLauncherFragment extends Fragment {
         return view;
     }
 
-    private class ActivityHolder extends RecyclerView.ViewHolder {
+    private class ActivityHolder extends RecyclerView.ViewHolder
+                                implements View.OnClickListener {
         private ResolveInfo mResolveInfo;
         private TextView mNameTextView;
 
         public ActivityHolder(View itemView) {
             super(itemView);
             mNameTextView = (TextView) itemView;
+            mNameTextView.setOnClickListener(this);
         }
 
         public void bindActivity(ResolveInfo resolveInfo) {
@@ -62,6 +65,16 @@ public class AppsLauncherFragment extends Fragment {
             PackageManager pm = getActivity().getPackageManager();
             String appName = mResolveInfo.loadLabel(pm).toString();
             mNameTextView.setText(appName);
+        }
+
+        @Override
+        public void onClick(View v) {
+            ActivityInfo activityInfo = mResolveInfo.activityInfo;
+            Intent intent = new Intent(Intent.ACTION_MAIN)
+                    .setClassName(activityInfo.applicationInfo.packageName,
+                            activityInfo.name);
+
+            startActivity(intent);
         }
     }
 
@@ -76,7 +89,7 @@ public class AppsLauncherFragment extends Fragment {
         public ActivityHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
             View view = layoutInflater.inflate(android.R.layout.simple_list_item_1,
-                                                parent, false);
+                    parent, false);
             return new ActivityHolder(view);
         }
 
